96ed5a5f8df43eba9f4e938e0ea10126c032cb87
kelexine <frankiekelechi@gmail.com>
2023-08-12 19:10:48 +0100

    changed branch, renamed Pixi3-10_WiFi to Pixi3  and modified some files

---
+++ AndroidProducts.mk
@@ -0,0 +1,10 @@
+#
+# Copyright (C) 2023 The Android Open Source Project
+# Copyright (C) 2023 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/omni_Pixi3.mk
+
