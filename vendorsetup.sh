95ed6bb402ff2f5bc7a9fcbe7f002b65597a6d9d
YZ-Bruh <131263805+YZ-Bruh@users.noreply.github.com>
2023-08-13 09:19:37 +0300

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -5,6 +5,6 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-add_lunch_combo omni_Pixi3-user
-add_lunch_combo omni_Pixi3-userdebug
-add_lunch_combo omni_Pixi3-eng
+add_lunch_combo omni_Pixi3-10_WiFi-user
+add_lunch_combo omni_Pixi3-10_WiFi-userdebug
+add_lunch_combo omni_Pixi3-10_WiFi-eng
